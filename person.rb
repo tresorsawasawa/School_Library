bba01e00fd38bc46a3462f720657369fb493e559
tresorsawasawa <tresorsawasawa@gmail.com>
2022-04-11 20:13:50 +0200

    Add can_use_services? && is_of_age? methods

--- person.rb
+++ person.rb
@@ -8,4 +8,14 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
+
+  def can_use_services?
+    is_of_age? || parent_permission
+  end
+
+  private
+
+  def is_of_age?
+    @age >= 18
+  end
 end
