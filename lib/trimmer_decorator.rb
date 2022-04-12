63be111e931bb8e56e85be9cf06998b84349c9fb
tresorsawasawa <tresorsawasawa@gmail.com>
2022-04-12 16:49:07 +0200

    Inherits the TrimmerDecorator class from BaseDecorator class and  Fix a linter error of new line at the end

--- lib/trimmer_decorator.rb
+++ lib/trimmer_decorator.rb
@@ -1,8 +1,8 @@
 require_relative 'base_decorator'
 
-class TrimmerDecorator
+class TrimmerDecorator < BaseDecorator
   def correct_name
-    if @nameable.correct_name.lenght < 10
+    if @nameable.correct_name.length < 10
       @nameable.correct_name
     else
       @nameable.correct_name[0, 10]
