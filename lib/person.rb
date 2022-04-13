aee2bbd7dfe1ef5d2a46c248ea9ebd27fb3a3d1d
tresorsawasawa <tresorsawasawa@gmail.com>
2022-04-13 23:34:50 +0200

    correct a typo error

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 require_relative 'rental'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rental
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
