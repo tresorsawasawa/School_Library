2ecfe5e288335c6c90d2443509d5171082f500d8
tresorsawasawa <tresorsawasawa@gmail.com>
2022-04-21 12:23:32 +0200

    Remove unnecessary code line

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,8 @@ class Student < Person
   attr_reader :classroom
   attr_accessor :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission)
+  def initialize(age, name, parent_permission)
     super(age, name, parent_permission: parent_permission)
-    # @classroom = classroom
   end
 
   def play_hooky
